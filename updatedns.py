be4532a83170c77b57c3b802f605ea201418c2cb
KarstonK <48630944+KarstonK@users.noreply.github.com>
2022-05-28 11:51:15 -0400

    Update updatedns.py

--- updatedns.py
+++ updatedns.py
@@ -9,7 +9,24 @@ waitTime = 30
 from config import *
 import requests
 import json
+from linode_api4 import LinodeClient
+from linode_api4.objects.domain import Domain, DomainRecord
+import ipaddress
 
-ipv4 = requests.get(ipv4Link, verify = True)
-ipv6 = requests.get(ipv6Link, verify = True)
+client = LinodeClient(apiToken)
 
+while true:
+    ipv4 = requests.get(ipv4Link, verify = True)
+    ipv6 = requests.get(ipv6Link, verify = True)
+
+    if ((ipv4 != ipv4Old) or (ipv4 != ipv4Old)):
+        for record in Domain(client, domainId).records:
+            if (':' in record.target):
+                if (ipv6 != ipv6Old):
+                    DomainRecord().target = 
+
+            else:
+                if ((ipv4 != ipv4Old) and (record.target == ipv4Old)):
+                    DomainRecord().target = ipv4
+
+    wait(waitTime)
